c47a9710fc61bf40425797812cbc68ece6ca439b
t.viet <ta.viet@foodpanda.com>
2023-09-28 11:25:03 +0800

    Add a comment for provider test

--- provider/providertest/provider_test.go
+++ provider/providertest/provider_test.go
@@ -17,7 +17,7 @@ func TestProvider(t *testing.T) {
 		ProviderBaseURL: "http://localhost:8080",
 		Provider:        "pact-poc-provider",
 		ProviderVersion: "pact-proc-provider-v1.0",
-		PactDirs:        []string{"../../pacts"},
+		PactDirs:        []string{"../../pacts"}, // if we're using a Pact broker such as pact flow, this is not needed
 		StateHandlers: models.StateHandlers{
 			"discount #1 exists": func(setup bool, state models.ProviderState) (models.ProviderStateResponse, error) {
 				return models.ProviderStateResponse{}, nil
