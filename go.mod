ba898412585b7cefeeb11cf9e4548a3cc112b78e
t.viet <ta.viet@foodpanda.com>
2023-09-29 10:23:36 +0800

    Update provider

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/AnhTaFP/pact-poc
 go 1.20
 
 require (
+	github.com/gorilla/mux v1.8.0
+	github.com/mattn/go-sqlite3 v1.14.17
 	github.com/pact-foundation/pact-go/v2 v2.0.1
 	github.com/stretchr/testify v1.8.3
 )
--- provider/main.go
+++ provider/main.go
@@ -1,34 +1,93 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
 )
 
+type discount struct {
+	ID          int     `json:"id"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Type        string  `json:"type"`
+	Value       float64 `json:"value"`
+}
+
 func main() {
-	type discount struct {
-		ID          int      `json:"id"`
-		Title       string   `json:"title"`
-		Description string   `json:"description"`
-		Type        string   `json:"type"`
-		Value       float64  `json:"value"`
-		Vendors     []string `json:"vendors"`
+	db, err := initDb()
+	if err != nil {
+		log.Fatal("error initiating db:", err.Error())
 	}
 
-	http.HandleFunc("/discounts/1", func(writer http.ResponseWriter, request *http.Request) {
-		d := discount{
-			ID:          1,
-			Title:       "5.8% off",
-			Description: "58th Singaporean National Day discount",
-			Type:        "percentage",
-			Value:       5.8,
-			Vendors:     []string{"vendor1", "vendor2"},
+	r := mux.NewRouter()
+
+	r.HandleFunc("/discounts", func(w http.ResponseWriter, r *http.Request) {
+		var d discount
+		if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
-		b, _ := json.Marshal(d)
 
-		writer.Header().Set("Content-Type", "application/json")
-		writer.Write(b)
-	})
+		if err := insert(db, d); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-	http.ListenAndServe("localhost:8080", nil)
+		w.WriteHeader(http.StatusCreated)
+	}).Methods("POST")
+
+	r.HandleFunc("/discounts", func(w http.ResponseWriter, r *http.Request) {
+
+	}).Methods("GET")
+
+	r.HandleFunc("/discounts/{id}", func(w http.ResponseWriter, r *http.Request) {
+
+	}).Methods("GET")
+
+	r.HandleFunc("/discounts/{id}", func(w http.ResponseWriter, r *http.Request) {
+
+	}).Methods("PUT")
+
+	r.HandleFunc("/discounts/{id}", func(w http.ResponseWriter, r *http.Request) {
+
+	}).Methods("DELETE")
+
+	if err := http.ListenAndServe("localhost:8080", r); err != nil {
+		log.Fatal("error occured:", err.Error())
+	}
+}
+
+func initDb() (*sql.DB, error) {
+	const file = "discounts.db"
+	const createDb = `
+CREATE TABLE IF NOT EXISTS discounts (
+  id INTEGER NOT NULL PRIMARY KEY,
+  title TEXT NOT NULL,
+  description TEXT NOT NULL,
+  type VARCHAR(25) NOT NULL,
+  value DECIMAL(10,5) NOT NULL
+);
+`
+
+	db, err := sql.Open("sqlite3", file)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := db.Exec(createDb); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func insert(db *sql.DB, d discount) error {
+	_, err := db.Exec("INSERT INTO discounts VALUES(NULL, ?,?,?,?)", d.Title, d.Description, d.Type, d.Value)
+
+	return err
 }
